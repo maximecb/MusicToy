54b19a7ad407314690c282eea262a9b90352b7f9
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-02-02 16:28:50 -0500

    - Changed old copyright block to BSD license - OSS FTW, ~PLUR~

--- mixer.js
+++ mixer.js
@@ -1,16 +1,37 @@
 /*****************************************************************************
 *
-* Copyright (C) 2012 Maxime Chevalier-Boisvert
-* 
-* The source code contained within this file was written by Maxime
-* Chevalier-Boisvert. Duplication or redistribution of this code
-* without prior authorization from the author constitutes copyright
-* violation.
+*  This file is part of the MusicToy project. The project is
+*  distributed at:
+*  https://github.com/maximecb/MusicToy
 *
-* For more information about this program, please e-mail the
-* author, Maxime Chevalier-Boisvert at:
-* maximechevalierb /at/ gmail /dot/ com
-* 
+*  Copyright (c) 2012, Maxime Chevalier-Boisvert. All rights reserved.
+*
+*  This software is licensed under the following license (Modified BSD
+*  License):
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions are
+*  met:
+*   1. Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*   2. Redistributions in binary form must reproduce the above copyright
+*      notice, this list of conditions and the following disclaimer in the
+*      documentation and/or other materials provided with the distribution.
+*   3. The name of the author may not be used to endorse or promote
+*      products derived from this software without specific prior written
+*      permission.
+*
+*  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+*  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+*  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+*  NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+*  NOT LIMITED TO PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+*  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
 *****************************************************************************/
 
 /**
